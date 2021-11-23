fbe769cdb280d701c32172cab8f0f50d96f6cd38
Matous <bralor92@email.cz>
2021-11-23 20:12:31 +0100

    recommender/views.py: query and dataframe feature added

--- books/recommender/views.py
+++ books/recommender/views.py
@@ -1,7 +1,10 @@
 from django.db.models import Q
 from django.views.generic import TemplateView, ListView
 
+import pandas
+
 from .models import Collection
+from recommender.api import run_recommender
 
 
 class HomePageView(TemplateView):
@@ -13,19 +16,16 @@ class SearchResultsView(ListView):
     template_name: str = "search_results.html"
 
     def get_queryset(self):
+        """
+        Get query from the given form. Then process the input and render
+        the ResultView.
+        """
         query = self.request.GET.get('q')
-        object_list = Collection.objects.filter(
-            Q(book_title__icontains=query)
+        object_list = Collection.objects.all()
+
+        result = run_recommender(
+            query,
+            pandas.DataFrame(list(object_list.values()))
         )
 
-        # return get_recommendations(
-            # object_list = Collection.objects.filter(
-                # Q(book_title__icontains=query)
-            # )
-        # )
-
-        # pandas.DataFrame(list(object_list))
-
-
-        return object_list
-
+        return result
