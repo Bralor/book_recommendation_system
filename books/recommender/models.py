76116db9bde1ad4f5a3b769495461d018aac2997
Matous <bralor92@email.cz>
2021-11-25 12:23:55 +0100

    models.py: inline comments removed

--- books/recommender/models.py
+++ books/recommender/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 class Collection(models.Model):
     id = models.IntegerField(primary_key=True)
-    isbn = models.TextField(db_column='ISBN', blank=True, null=True)  # Field name made lowercase.
-    book_title = models.TextField(db_column='Book-Title', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    book_author = models.TextField(db_column='Book-Author', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    year_of_publication = models.TextField(db_column='Year-Of-Publication', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    publisher = models.TextField(db_column='Publisher', blank=True, null=True)  # Field name made lowercase.
-    image_url_s = models.TextField(db_column='Image-URL-S', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    image_url_m = models.TextField(db_column='Image-URL-M', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    image_url_l = models.TextField(db_column='Image-URL-L', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    user_id = models.TextField(db_column='User-ID', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    book_rating = models.TextField(db_column='Book-Rating', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    isbn = models.TextField(db_column='ISBN', blank=True, null=True)
+    book_title = models.TextField(db_column='Book-Title', blank=True, null=True)
+    book_author = models.TextField(db_column='Book-Author', blank=True, null=True)
+    year_of_publication = models.TextField(db_column='Year-Of-Publication', blank=True, null=True)
+    publisher = models.TextField(db_column='Publisher', blank=True, null=True)
+    image_url_s = models.TextField(db_column='Image-URL-S', blank=True, null=True)
+    image_url_m = models.TextField(db_column='Image-URL-M', blank=True, null=True)
+    image_url_l = models.TextField(db_column='Image-URL-L', blank=True, null=True)
+    user_id = models.TextField(db_column='User-ID', blank=True, null=True)
+    book_rating = models.TextField(db_column='Book-Rating', blank=True, null=True)
 
     class Meta:
         managed = False
